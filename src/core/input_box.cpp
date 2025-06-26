d7139bc266c4504fce65f51eb80477daa93f6058
Parv Shah <parv141206@gmail.com>
2025-06-26 11:22:45 +0530

    Fixed InputBox - Fixed the "wrap" rendering bug

--- src/core/input_box.cpp
+++ src/core/input_box.cpp
@@ -76,50 +76,76 @@ void InputBox::handle_input(char ch) {
 		break;
 	}
 }
+
+
 void InputBox::render(ScreenBuffer& buffer, int x, int y, int w, int h) const {
-	const std::string border_style = ansi::RESET;
-	const std::string text_style = ansi::RESET;
-	const std::string cursor_style = ansi::INVERSE;
+    const std::string border_style = ansi::RESET;
+    const std::string text_style = ansi::RESET;
+    const std::string cursor_style = ansi::INVERSE;
 
-	const int innerW = w - 2;
-	int scroll_offset = 0;
-	if (innerW > 0 && !wrap) {
-		if (cursor >= innerW) {
-			scroll_offset = cursor - innerW + 1;
-		}
-	}
+    const int innerW = w - 2;
 
-	for (int i = 0; i < h; ++i) {
-		for (int j = 0; j < w; ++j) {
-			const int current_x = x + j;
-			const int current_y = y + i;
+    for (int i = 0; i < h; ++i) {  
+        for (int j = 0; j < w; ++j) { 
+            const int current_x = x + j;
+            const int current_y = y + i;
 
-			const bool is_top = (i == 0);
-			const bool is_bottom = (i == h - 1);
-			const bool is_left = (j == 0);
-			const bool is_right = (j == w - 1);
+            const bool is_top = (i == 0);
+            const bool is_bottom = (i == h - 1);
+            const bool is_left = (j == 0);
+            const bool is_right = (j == w - 1);
 
-			if (is_top && is_left) { buffer.set_cell(current_x, current_y, ansi::tl, border_style); continue; }
-			if (is_top && is_right) { buffer.set_cell(current_x, current_y, ansi::tr, border_style); continue; }
-			if (is_bottom && is_left) { buffer.set_cell(current_x, current_y, ansi::bl, border_style); continue; }
-			if (is_bottom && is_right) { buffer.set_cell(current_x, current_y, ansi::br, border_style); continue; }
-			if (is_top || is_bottom) { buffer.set_cell(current_x, current_y, ansi::h, border_style); continue; }
-			if (is_left || is_right) { buffer.set_cell(current_x, current_y, ansi::v, border_style); continue; }
+            if (is_top && is_left) { buffer.set_cell(current_x, current_y, ansi::tl, border_style); continue; }
+            if (is_top && is_right) { buffer.set_cell(current_x, current_y, ansi::tr, border_style); continue; }
+            if (is_bottom && is_left) { buffer.set_cell(current_x, current_y, ansi::bl, border_style); continue; }
+            if (is_bottom && is_right) { buffer.set_cell(current_x, current_y, ansi::br, border_style); continue; }
+            if (is_top || is_bottom) { buffer.set_cell(current_x, current_y, ansi::h, border_style); continue; }
+            if (is_left || is_right) { buffer.set_cell(current_x, current_y, ansi::v, border_style); continue; }
 
-			const int content_col = j - 1;
-			wchar_t char_to_draw = L' ';
-			std::string style_to_use = text_style;
+            const int content_row = i - 1;
+            const int content_col = j - 1;
+            wchar_t char_to_draw = L' ';
+            std::string style_to_use = text_style;
 
-			if (scroll_offset + content_col < text.length()) {
-				char_to_draw = text[scroll_offset + content_col];
-			}
+            if (wrap) {
+                int text_pos = content_row * innerW + content_col;
+                if (text_pos < text.length()) {
+                    char_to_draw = text[text_pos];
+                }
+            }
+            else {
+                if (content_row == 0) {
+                    int scroll_offset = 0;
+                    if (cursor >= innerW) {
+                        scroll_offset = cursor - innerW + 1;
+                    }
+                    int text_pos = scroll_offset + content_col;
+                    if (text_pos < text.length()) {
+                        char_to_draw = text[text_pos];
+                    }
+                }
+            }
 
-			const int cursor_in_view_col = cursor - scroll_offset;
-			if (active && content_col == cursor_in_view_col) {
-				style_to_use = cursor_style;
-			}
+            if (active) {
+                int cursor_row_actual = wrap ? (cursor / innerW) : 0;
+                int cursor_col_actual;
+                if (wrap) {
+                    cursor_col_actual = cursor % innerW;
+                }
+                else {
+                    int scroll_offset = 0;
+                    if (cursor >= innerW) {
+                        scroll_offset = cursor - innerW + 1;
+                    }
+                    cursor_col_actual = cursor - scroll_offset;
+                }
 
-			buffer.set_cell(current_x, current_y, std::string(1, char_to_draw), style_to_use);
-		}
-	}
+                if (content_row == cursor_row_actual && content_col == cursor_col_actual) {
+                    style_to_use = cursor_style;
+                }
+            }
+
+            buffer.set_cell(current_x, current_y, std::string(1, char_to_draw), style_to_use);
+        }
+    }
 }
